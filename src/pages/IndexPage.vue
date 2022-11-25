f1156aaca007687928bc44931dca174740a242f7
Ojvar <MOjvar.AmirHossein@Gmail.com>
2022-11-25 10:17:49 +0330

    init project

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+
+<div class="colums text-center">
+
+    <h2>Welcome to </h2>
+    <h1>MyMessenger</h1>
+
+</div>
+
+</q-page>
+</template>
+
+<script setup lang="ts">
+</script>
